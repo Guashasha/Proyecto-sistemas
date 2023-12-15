b93bbcda19ce1b5c1a771d4c412781640ab2c861
Guashasha <davidcarrionr00@hotmail.com>
2023-12-15 02:22:09 -0600

    version semifinal de documentación

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ El algoritmo NAT (*Network Addres Translation*) es un mecanismo utilizado por ro
 La implementación se hará con procesos en vez de redes, cada proceso teniendo su propia identidad, no podrán interactuar directamente entre ellos, y un proceso que emulará un router, recibiendo y mandando mensajes entre procesos al proceso objetivo.
 
 ## Implementación
-Cada proceso tiene su propio codigo en C, el cual es el que le dará su identidad unica, y a traves del cual se mandarán los mensajes al router.
+Cada proceso tiene su propio codigo en C, el cual es el que le dará su identidad unica, y a traves del cual se mandarán y recibirán los mensajes.
 
-Ademas de los procesos que mandan mensajes hay procesos que reciben los mensajes y mandan una respuesta, éstos también tendrán su propio archivo en C.
+El proceso principal (router.c) emula al router, y su trabajo es recibir mensajes de las computadoras, mandarlos al servidor, recibir una respuesta del servidor y mandarlos de regreso a la computadora que lo mandó en primer lugar.
+Despues de iniciar las conexiones necesarias, espera a un cliente (pc) que mande un mensaje, seguido de ello manda el mensaje al servidor, el cual lo modifica y lo manda de regreso al router, éste lo manda directamente de regreso al cliente que mandó el mensaje.
 
-El proceso principal, el que emula al router, al recibir mensajes guardará la información del Emisor y del Receptor, y enviará el mensaje al receptor, recibirá otro mensaje y lo transmitirá al Emisor original.
+El proceso que actua como el servidor espera a recibir un mensaje, el cual llega solamente de parte del router, posteriormente convierte a mayusculas el mensaje y lo envía de regreso al router.
+
+Los procesos "clientes" (pc) piden al usuario una cadena, la mandan al router y esperan una respuesta para despues imprimirla.
--- router.c
+++ router.c
@@ -1,62 +1,58 @@
 // router.c
 #include "mensaje.h"
+#include <ctype.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <ctype.h>
 
 int main(int argc, char *argv[]) {
-    char mensaje[256];
-    int router_socket = socket(AF_INET, SOCK_STREAM, 0);
+  char mensaje[256];
+  int router_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in raddr;
-    raddr.sin_family = AF_INET;
-    raddr.sin_port = htons(8080);
-    raddr.sin_addr.s_addr = INADDR_ANY;
+  struct sockaddr_in raddr;
+  raddr.sin_family = AF_INET;
+  raddr.sin_port = htons(8080);
+  raddr.sin_addr.s_addr = INADDR_ANY;
 
-    struct sockaddr_in saddr;
-    saddr.sin_family = AF_INET;
-    saddr.sin_port = htons(8081);
-    saddr.sin_addr.s_addr = INADDR_ANY;
+  struct sockaddr_in saddr;
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = htons(8081);
+  saddr.sin_addr.s_addr = INADDR_ANY;
 
-    bind(router_socket, (struct sockaddr*)&raddr, sizeof(raddr));
+  bind(router_socket, (struct sockaddr *)&raddr, sizeof(raddr));
 
-    listen(router_socket, 5);
+  listen(router_socket, 5);
 
-    int client_socket;
-    int server_socket;
-    int connection;
-    struct Mensaje m;
+  int client_socket;
+  int server_socket;
+  int connection;
+  struct Mensaje m;
 
-    while (1) {
-        client_socket = accept(router_socket, NULL, NULL);
-        recv(client_socket, &m, sizeof(m), 0);
-        printf("Recibido mensaje de la computadora: %s\n", m.msg);
+  while (1) {
+    client_socket = accept(router_socket, NULL, NULL);
+    recv(client_socket, &m, sizeof(m), 0);
+    printf("Recibido mensaje de la computadora: %s\n", m.msg);
 
-        // Convertir a mayúsculas
-        for (int i = 0; i < sizeof(m.msg); i++) {
-            m.msg[i] = toupper(m.msg[i]);
-        }
+    // Conectar con el servidor, enviar y recibir mensaje
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    connection =
+        connect(server_socket, (struct sockaddr *)&saddr, sizeof(saddr));
+    send(server_socket, &m, sizeof(m), 0);
+    recv(server_socket, &m, sizeof(m), 0);
+    printf("Recibido mensaje del servidor: %s\n", m.msg);
 
-        // Conectar con el servidor, enviar y recibir mensaje
-        server_socket = socket(AF_INET, SOCK_STREAM, 0);
-        connection = connect(server_socket, (struct sockaddr*)&saddr, sizeof(saddr));
-        send(server_socket, &m, sizeof(m), 0);
-        recv(server_socket, &m, sizeof(m), 0);
-        printf("Recibido mensaje del servidor: %s\n", m.msg);
+    // Enviar mensaje a la computadora
+    send(client_socket, &m, sizeof(m), 0);
 
-        // Enviar mensaje a la computadora
-        send(client_socket, &m, sizeof(m), 0);
+    // Cerrar el socket del servidor
+    close(server_socket);
 
-        // Cerrar el socket del servidor
-        close(server_socket);
+    // Cerrar el socket de la computadora
+    close(client_socket);
+  }
 
-        // Cerrar el socket de la computadora
-        close(client_socket);
-    }
-
-    return 0;
+  return 0;
 }
